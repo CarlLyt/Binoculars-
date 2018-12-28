d01bfd67f291679863801c708e46f32936ad769a
CarlLyt <42412722+CarlLyt@users.noreply.github.com>
2018-12-28 23:26:58 +0800

    Add files via upload
    旋转角度不精确，旋转方向大体辨别出来

---
+++ ConfirmFlatV1/project/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<opencv2/opencv.hpp>
+#include<vector>
+#include<Camera.h>
+using namespace std;
+#define imageWidth 640
+#define imageHight 480
+
+int main() {
+	Camera cap;
+	cap.start();
+	//cap.Getpic();
+
+
+	return 0;
+}
